3c0ee498dc5f68e2878c61632e8759210a3da41f
MO-JA-SH <mohammedjafer228@gmail.com>
2026-08-25 16:09:21 +0300

    revert(android): restore Android version to 1.5.1

--- apk+ublock/app/build.gradle.kts
+++ apk+ublock/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ubour.vpn"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.6.0"
+        versionCode = 7
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
